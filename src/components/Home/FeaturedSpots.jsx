4919abf22ea2a438939bda4cf1159b6ac14943e6
John Mark Larga <53073416+jmlarga@users.noreply.github.com>
2026-07-08 01:14:15 +0800

    Update FeaturedSpots.jsx

--- src/components/Home/FeaturedSpots.jsx
+++ src/components/Home/FeaturedSpots.jsx
@@ -7,6 +7,7 @@ const FeaturedSpots = () => {
 
   useEffect(() => {
     fetch(
+      "https://e-gala-api.freedev.app/wp-json/wp/v2/tourist_spots?_embed&per_page=6"
     )
       .then((res) => res.json())
       .then((data) => {
