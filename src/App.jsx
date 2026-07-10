0d168b43af491babcaffe1dd6f2d6f43147e9feb
John Mark Larga <johnmarklarga@gmail.com>
2026-07-10 18:40:34 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ function App() {
                 setIsMenuOpen={setIsMenuOpen}
             />
 
-		 <main className="flex-1 overflow-y-auto p-4">
+		 <main className="flex-1 overflow-y-auto py-4">
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/spots" element={<Spots />} />
