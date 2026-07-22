7f9e29fdec3c10454053744122d5ca0807ae52d3
John Mark Larga <johnmarklarga@gmail.com>
2026-07-22 16:58:14 +0800

    fixed reviews

--- src/services/AppReviewService.js
+++ src/services/AppReviewService.js
@@ -1,4 +1,15 @@
+import axios from "axios";
+
 import API from "../config/api";
+
+export const submitAppReview = async (data) => {
+    const response = await axios.post(
+        `${API}/e-gala/v1/app-review`,
+        data
+    );
+
+    return response.data;
+};
 
 export async function getAppReviews() {
 
